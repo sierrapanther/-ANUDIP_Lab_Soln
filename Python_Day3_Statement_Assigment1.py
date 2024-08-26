472145cb1eec40cfb6d6c2f0edc6606785031e40
Sahil Patel <98147050+sierrapanther@users.noreply.github.com>
2024-08-26 20:32:06 +0530

    Add files via upload

---
+++ Python_Day3_Statement_Assigment1.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+
+result = "Even" if num % 2 == 0 else "Odd"
+
+print(f"The number {num} is {result}.")
---
+++ Python_Day3_Statement_Assigment2.py
@@ -0,0 +1,6 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+num1, num2 = num2, num1
+
+print(f"After swapping:\nFirst number: {num1}\nSecond number: {num2}")
